95fa2acd84915ddde2d73402119a5fa642e9624b
chen1501862055 <1501862055@qq.com>
2021-10-21 10:44:11 +0800

    Commit message

---
+++ 11/11.28/format.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+#define MAX 20
+
+char *s_gets(char *st, int n);
+
+int main() {
+    char first[MAX];
+    char last[MAX];
+    char formal[2 * MAX + 10];
+    double prize;
+
+    puts("Enter your first name:");
+    s_gets(first, MAX);
+    puts("Enter your last name:");
+    s_gets(last, MAX);
+    puts("Enter your prize money:");
+    scanf("%lf", &prize);
+    sprintf(formal, "%s, %-19s: $%6.2f\n", last, first, prize);  //该函数获取输入,并将其格式化为标准形式,然后把格式化后的字符串存在formal中
+    puts(formal);
+
+    return 0;
+}
+
+char *s_gets(char *st, int n) {
+    char *ret_val;
+    int i = 0;
+
+    ret_val = fgets(st, n, stdin);
+    if (ret_val) {
+        while (st[i] != '\n' && st[i] != '\0')
+            i++;
+        if (st[i] == '\n')
+            st[i] = '\0';
+        else
+            while (getchar() != '\n')
+                continue;
+    }
+    return ret_val;
+}
