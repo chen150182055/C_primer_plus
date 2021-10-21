b55ef40c1cdaa795a7cc81bb430ff035b41d5001
chenxihong123 <1501862055@qq.com>
2021-10-21 15:51:44 +0800

    Commit message

---
+++ 11/11.31/cmake-build-debug/CMakeFiles/11_31.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11_31.exe"
+  "11_31.exe.manifest"
+  "11_31.pdb"
+  "CMakeFiles/11_31.dir/repeat.c.obj"
+  "lib11_31.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/11_31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
