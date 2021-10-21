f9bf9ff6252f8d6a5f38783d207ad6079f411e5b
chenxihong123 <1501862055@qq.com>
2021-10-21 15:30:47 +0800

    Commit message

---
+++ 11/11.30/cmake-build-debug/CMakeFiles/11_30.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "11_30.exe"
+  "11_30.exe.manifest"
+  "11_30.pdb"
+  "CMakeFiles/11_30.dir/mod_str.c.obj"
+  "lib11_30.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/11_30.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
