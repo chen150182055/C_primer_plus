cdc453707266a4afe9a29cca3d96e6f825aa9407
chenxihong123 <1501862055@qq.com>
2021-10-21 19:53:46 +0800

    Commit message

---
+++ 12/12.3/cmake-build-debug/CMakeFiles/12_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "12_3.exe"
+  "12_3.exe.manifest"
+  "12_3.pdb"
+  "CMakeFiles/12_3.dir/main.c.obj"
+  "lib12_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/12_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
