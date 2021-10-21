71201f55b889e02670905a200dffc7b18037d638
chenxihong123 <1501862055@qq.com>
2021-10-21 15:56:48 +0800

    Commit message

--- 11/11.31/repeat.c
+++ 11/11.31/repeat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { //第一个参数是命令行中字符串的数量,第二个参数是用来存放每个字符串的地址（指针数组）
     int count;
 
     printf("The command line has %d arguments:\n", argc - 1);
