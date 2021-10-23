84f4917f0064ff3250362bdd0f58289098e5714e
chenxihong123 <1501862055@qq.com>
2021-10-23 10:14:34 +0800

    13

---
+++ 13/13.4/cmake-build-debug/CMakeFiles/13_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "13_4.exe"
+  "13_4.exe.manifest"
+  "13_4.pdb"
+  "CMakeFiles/13_4.dir/reverse.c.obj"
+  "lib13_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/13_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
