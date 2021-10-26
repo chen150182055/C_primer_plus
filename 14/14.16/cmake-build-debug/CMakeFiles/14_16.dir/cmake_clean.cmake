5bd4fb574eb9043c1e54e22c0b9b8c0371dab066
chenxihong123 <1501862055@qq.com>
2021-10-26 10:35:59 +0800

    16

---
+++ 14/14.16/cmake-build-debug/CMakeFiles/14_16.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14_16.exe"
+  "14_16.exe.manifest"
+  "14_16.pdb"
+  "CMakeFiles/14_16.dir/func_ptr.c.obj"
+  "lib14_16.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/14_16.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
