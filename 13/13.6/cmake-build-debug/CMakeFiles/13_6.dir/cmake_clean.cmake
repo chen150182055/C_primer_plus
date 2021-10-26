3a7efcdbd513014b7a027b83052247ebda8b95e0
chenxihong123 <1501862055@qq.com>
2021-10-26 08:21:11 +0800

    13.6

---
+++ 13/13.6/cmake-build-debug/CMakeFiles/13_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "13_6.exe"
+  "13_6.exe.manifest"
+  "13_6.pdb"
+  "CMakeFiles/13_6.dir/randbin.c.obj"
+  "lib13_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/13_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
