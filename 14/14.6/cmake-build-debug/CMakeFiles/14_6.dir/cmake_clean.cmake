172013b7294a4f1a3ce9f5bae8440af3e5a5d881
chen1501862055 <1501862055@qq.com>
2021-10-28 11:01:52 +0800

    Commit message

---
+++ 14/14.6/cmake-build-debug/CMakeFiles/14_6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14_6.exe"
+  "14_6.exe.manifest"
+  "14_6.pdb"
+  "CMakeFiles/14_6.dir/funds2.c.obj"
+  "lib14_6.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/14_6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
