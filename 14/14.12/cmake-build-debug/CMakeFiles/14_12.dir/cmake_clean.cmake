a7742dad77c3930142f92431f7032777be7ff2a8
chenxihong123 <1501862055@qq.com>
2021-10-28 15:58:10 +0800

    Comiit message

---
+++ 14/14.12/cmake-build-debug/CMakeFiles/14_12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14_12.exe"
+  "14_12.exe.manifest"
+  "14_12.pdb"
+  "CMakeFiles/14_12.dir/flexmemb.c.obj"
+  "lib14_12.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/14_12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
