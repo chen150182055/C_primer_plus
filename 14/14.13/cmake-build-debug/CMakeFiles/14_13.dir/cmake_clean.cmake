7f4d6d61b24d76f6a7909260806128bafe0274cf
chenxihong123 <1501862055@qq.com>
2021-10-28 16:18:26 +0800

    Commit message

---
+++ 14/14.13/cmake-build-debug/CMakeFiles/14_13.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14_13.exe"
+  "14_13.exe.manifest"
+  "14_13.pdb"
+  "CMakeFiles/14_13.dir/funds4.c.obj"
+  "lib14_13.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/14_13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 14/14.14/cmake-build-debug/CMakeFiles/14_14.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14_14.exe"
+  "14_14.exe.manifest"
+  "14_14.pdb"
+  "CMakeFiles/14_14.dir/main.c.obj"
+  "lib14_14.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/14_14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
