d01168923936edc3ce2169ca3665e729ca4d5f84
chen1501862055 <1501862055@qq.com>
2021-10-30 11:33:35 +0800

    15

---
+++ 15/15.1/cmake-build-debug/CMakeFiles/15_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_1.exe"
+  "15_1.exe.manifest"
+  "15_1.pdb"
+  "CMakeFiles/15_1.dir/binbit.c.obj"
+  "lib15_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/15_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
