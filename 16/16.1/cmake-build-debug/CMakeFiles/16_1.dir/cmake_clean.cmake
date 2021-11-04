5d3d5e9df7d176c6a91c8ae9064730c16be49104
chen1501862055 <1501862055@qq.com>
2021-11-04 12:10:56 +0800

    16Commit message

---
+++ 16/16.1/cmake-build-debug/CMakeFiles/16_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "16_1.exe"
+  "16_1.exe.manifest"
+  "16_1.pdb"
+  "CMakeFiles/16_1.dir/main.c.obj"
+  "lib16_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/16_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
