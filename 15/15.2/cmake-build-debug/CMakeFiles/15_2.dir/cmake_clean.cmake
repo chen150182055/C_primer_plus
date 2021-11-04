6ca0a7e71b17c67b0b6d2c3ec5a682a63ee6feac
chen1501862055 <1501862055@qq.com>
2021-11-04 10:13:15 +0800

    Commit message

---
+++ 15/15.2/cmake-build-debug/CMakeFiles/15_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_2.exe"
+  "15_2.exe.manifest"
+  "15_2.pdb"
+  "CMakeFiles/15_2.dir/invert4.c.obj"
+  "lib15_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/15_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
