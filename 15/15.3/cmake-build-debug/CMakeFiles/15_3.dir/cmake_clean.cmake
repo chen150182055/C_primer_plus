f1ce7a6e50990ce081125b9c471830953cd32246
chen1501862055 <1501862055@qq.com>
2021-11-04 10:49:00 +0800

    Commit message

---
+++ 15/15.3/cmake-build-debug/CMakeFiles/15_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_3.exe"
+  "15_3.exe.manifest"
+  "15_3.pdb"
+  "CMakeFiles/15_3.dir/fields.c.obj"
+  "lib15_3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/15_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 15/15.4/cmake-build-debug/CMakeFiles/15_4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "15_4.exe"
+  "15_4.exe.manifest"
+  "15_4.pdb"
+  "CMakeFiles/15_4.dir/main.c.obj"
+  "lib15_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/15_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
